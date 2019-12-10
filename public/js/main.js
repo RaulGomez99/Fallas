cf0557c123bfefe5cdb691a1a344d7d00e8031d8
RaulGomez99 <gomezlopezraul1999@gmail.com>
2019-12-10 18:45:04 +0100

    Fallas

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 window.onload=init;
-const url = "http://mapas.valencia.es/lanzadera/opendata/TRA_REGULADORES/JSON";
+const url = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
 
 function init(){
     document.getElementById("botonBuscar").addEventListener("click", buscar);
@@ -12,23 +12,14 @@ function buscar(){
     fetchBusc.then(res =>{
         return res.json();
     }).then(respuesta =>  {
-        
-        let tabla = document.createElement("table");
-        let th = document.createElement("tr");
-        th.innerHTML=`<th>Descripción</th><th>Coordenadas</th>`;
-        tabla.appendChild(th);
-        respuesta.features.filter(filtro).forEach(element => {
-            let tr = document.createElement("tr");
-            tr.innerHTML=`<td>${element.properties.descripcion}</td><td>${element.geometry.coordinates[0]}--${element.geometry.coordinates[1]}</td>`;
+        //respuesta.features.filter(filtro).forEach(element => {
+            respuesta.features.forEach(element => {
             console.log(element);
-           tabla.appendChild(tr);
         });
-        document.querySelector("content").innerHTML="";
-        document.querySelector("content").appendChild(tabla);
     })
 }
 
 function filtro(elemento){
-    let letra=document.querySelector(`input[type="text"]`).value.toUpperCase();
-    return elemento.properties.descripcion.startsWith(letra);
+    /*let letra=document.querySelector(`input[type="text"]`).value.toUpperCase();
+    return elemento.properties.descripcion.startsWith(letra);*/
 }

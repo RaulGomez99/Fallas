66cdb7058393e6402b2319a588d7e605b49e5698
RaulGomez99 <gomezlopezraul1999@gmail.com>
2019-12-10 19:57:00 +0100

    Probando coordenadas

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,27 @@
-window.onload=init;
+window.onload = init;
 const url = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
 
-function init(){
+function init() {
     document.getElementById("botonBuscar").addEventListener("click", buscar);
+    buscar();
 }
 var a;
 
-function buscar(){
+function buscar() {
     const fetchBusc = fetch(url);
 
-    fetchBusc.then(res =>{
+    fetchBusc.then(res => {
         return res.json();
-    }).then(respuesta =>  {
+    }).then(respuesta => {
         //respuesta.features.filter(filtro).forEach(element => {
-            respuesta.features.forEach(element => {
-            console.log(element);
+        respuesta.features.forEach(element => {
+          //  console.log(element);
+            document.querySelector("content").innerHTML+="<div class='falla'><img src="+element.properties.boceto+"><p>"+element.properties.nombre+"</p></div>";
         });
     })
 }
 
-function filtro(elemento){
+function filtro(elemento) {
     /*let letra=document.querySelector(`input[type="text"]`).value.toUpperCase();
     return elemento.properties.descripcion.startsWith(letra);*/
 }
